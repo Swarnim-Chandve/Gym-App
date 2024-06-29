8f046e305a6702ff2519dbe05f73aba52f14a582
Swarnim <swarnimchandve23@gmail.com>
2024-06-29 13:56:01 +0530

    firstcommit

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+import Hero from './components/Hero'
+import Generator from './components/Generator'
+import Workout from './components/Workout'
+import { generateWorkout } from './utils/functions'
+import RC from "./assets/RC.mp4"
+
+
+function App() {
+  const [workout, setWorkout] = useState(null)
+  const [poison, setPoison] = useState('individual')
+  const [muscles, setMuscles] = useState([])
+  const [goal, setGoal] = useState('strength_power')
+
+  function updateWorkout() {
+    if (muscles.length < 1) {
+      return
+    }
+    let newWorkout = generateWorkout({ poison, muscles, goal })
+    setWorkout(newWorkout)
+
+    window.location.href = '#workout'
+  }
+
+  return (
+    <main className='min-h-screen flex flex-col bg-gradient-to-r from-slate-800 to-slate-950 text-white text-sm sm:text-base'>
+   
+
+<video className='videoTag ' autoPlay loop muted>
+    <source src={RC} type='video/mp4' />
+</video>
+      <Hero className='absolute' />
+      <Generator
+        poison={poison}
+        setPoison={setPoison}
+        muscles={muscles}
+        setMuscles={setMuscles}
+        goal={goal}
+        setGoal={setGoal}
+        updateWorkout={updateWorkout}
+      />
+      {workout && (<Workout workout={workout} />)}
+    </main>
+  )
+}
+
+export default App
